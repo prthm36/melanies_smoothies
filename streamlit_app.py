c8b16b8292e8ad51d86d11d4d2e66b4c61e5259e
Pratham <kashyappratham000@gmail.com>
2025-01-09 09:50:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 # st.stop()
 # Convert snowpark dataframe to pandas dataframe
 pd_df = my_dataframe.to_pandas()
-st_dataframe = (pd_df)
-st.stop()
+#st_dataframe = (pd_df)
+#st.stop()
 ingredients_list = st.multiselect(
     'Choose  up to 5 ingredients  : '
     ,my_dataframe
@@ -31,8 +31,11 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen +' '
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
         st.subheader(fruit_chosen + 'Nutrition Information')
-    # # st.write (ingredients_string)
+        
+        # # st.write (ingredients_string)
     # my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
     #         values ('""" + ingredients_string + """','""" + name_on_order+"""')"""
 
